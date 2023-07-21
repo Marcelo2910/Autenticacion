487faf609787ef01c671a22c96881e5de986dbbb
Marcelo2910 <rammstolomeo@hotmail.com>
2023-07-21 09:37:21 -0600

    Encriptacion usando mongoose-encrypt y creando archivo env con dotenv

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
+
 
 const app = express()
 
@@ -15,8 +18,9 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const User = mongoose.model("User", userSchema)
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']})
 
+const User = mongoose.model("User", userSchema)
 
 app.get("/", function (req, res){
     res.render("home")
